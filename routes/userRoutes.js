450e88b32d2c4f1671a5dfb3c690968051207ef3
Arvind Prajapati <arvindprajapatijan86@gmail.com>
2025-11-09 11:36:42 +0530

    Add GET route for /api/user/login to fix Not Found error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,9 @@ router.post('/register', registerUser);
 // Login route
 router.post('/login', loginUser);
 
+// GET route for testing (returns a message)
+router.get('/login', (req, res) => {
+  res.json({ message: 'Login endpoint is working. Use POST to login.' });
+});
+
 export default router;
